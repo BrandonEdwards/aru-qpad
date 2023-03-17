3a9d6e8383ca51700f68b5f52a3a6d0bccb09589
Brandon Edwards <brandon.edwards144@gmail.com>
2023-03-17 10:47:05 -0400

    up

--- src/02-generate-detections.R
+++ src/02-generate-detections.R
@@ -11,6 +11,9 @@ library(locaR)
 
 ####### Read Data #################################
 
+filenames <- as.vector(read.csv("data/generated/filenames_wav.csv", header = FALSE)[,1])
+tags <- read.csv("data/raw/wildtrax_tags.csv"); names(tags)[1] <- "location"
+stations <- read.csv("data/raw/station_locs.csv")
 
 ####### Main Code #################################
 
