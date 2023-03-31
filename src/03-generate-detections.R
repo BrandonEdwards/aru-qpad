8c2a0bd8d112cc9e465434d638d9f9df3800ff7b
Brandon Edwards <brandon.edwards144@gmail.com>
2023-03-31 14:34:15 -0400

    update name of file, add some data wrangling

--- src/03-generate-detections.R
+++ src/03-generate-detections.R
@@ -1,19 +1,20 @@
 ####### Script Information ########################
 # Brandon P.M. Edwards
 # aru-qpad
-# 02-generate-detections.R
+# 03-generate-detections.R
 # Created March 2023
 # Last Updated March 2023
 
 ####### Import Libraries and External Files #######
 
 library(locaR)
+library(tuneR)
+library(seewave)
 
 ####### Read Data #################################
 
 filenames <- as.vector(read.csv("data/generated/filenames_wav.csv", header = FALSE)[,1])
 tags <- read.csv("data/raw/wildtrax_tags.csv"); names(tags)[1] <- "location"
-stations <- read.csv("data/raw/station_locs.csv")
 
 ####### Main Code #################################
 
@@ -31,28 +32,62 @@ for (i in 1:nrow(tags))
   }
 }
 
-# Create file list for each project's coordinates
-coords_filenames <- data.frame(project = unique(tags$array_name),
-                               filename = NA)
-i <- 1
-for (p in coords_filenames$project)
+# Get list of mic locations for each project
+mic_locations <- vector(mode = "list", length = length(unique(tags$array_name)))
+names(mic_locations) <- unique(tags$array_name)
+mic_matrix <- vector(mode = "list", length = length(unique(tags$array_name)))
+names(mic_matrix) <- unique(tags$array_name)
+for (m in names(mic_locations))
 {
-  if (grepl("SBL", p, fixed = TRUE))
+  temp <- read.csv(paste0("data/generated/mic_locations/completed/",
+                          m,
+                          ".csv"))
+  mic_locations[[m]] <- temp
+  
+  temp_matrix <- matrix(nrow = max(temp$Y), ncol = max(temp$X))
+  for (x in 1:max(temp$X))
   {
-    p_mod <- p
-    stringi::stri_sub(p_mod, 5, 4) <- "0"
-    coords <- stations[which(grepl(p_mod, stations$Station, fixed = TRUE)), ]
-    f <- tempfile()
-    coords_filenames$filename[i] <- f
-    coords <- coords[, 1:5]
-    write.table(coords, file = f,
-                row.names = FALSE, sep = ",")
+    for (y in 1:max(temp$Y))
+    {
+      temp_matrix[y,x] <- temp[which(temp$Y == y &
+                                       temp$X == x),
+                               "Station"]
+    }
   }
-  i <- i + 1
+  mic_matrix[[m]] <- temp_matrix
 }
 
-#' * for each project:
-#'   * create a new survey
+for (i in 1:nrow(tags))
+{
+  files <- filenames[which(grepl(tags$array[i], filenames, fixed = TRUE))]
+
+  sounds <- vector(mode = "list", length = nrow(mic_locations[[tags$array_name[i]]]))
+  names(sounds) <- paste0(substr(mic_locations[[tags$array_name[i]]]$Station, 1, 4),
+                          substr(mic_locations[[tags$array_name[i]]]$Station, 6, 
+                                 nchar(mic_locations[[tags$array_name[i]]]$Station)))
+  
+  for (s in names(sounds))
+  {
+    # This doesn't work, I need a way to just cross-reference tags to the appropriate file names
+    sounds[[s]] <- tuneR::readWave(filename = files[which(grepl(s, files, fixed = TRUE))])
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 survey_list <- vector(mode = "list", length = nrow(coords_filenames))
 names(survey_list) <- coords_filenames$project
