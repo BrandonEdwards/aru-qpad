aba9eabc9035263dc1dfcf0611c28a37e8817f78
Brandon Edwards <brandon.edwards144@gmail.com>
2023-12-12 13:08:47 -0500

    cleaning up some code here and there

--- src/00-build-stations-table.R
+++ src/00-build-stations-table.R
@@ -14,12 +14,7 @@ library(RSQLite)
 
 station_locs <- read.csv(file = "data/raw/station_locs.csv")
 
-
 ####### Main Code #################################
-
-# Create a connection to a database
-db <- DBI::dbConnect(RSQLite::SQLite(),
-                     "data/generated/localization.db")
 
 station_locs$Project <- sub("-.*", "", station_locs$Station)
 sl_red <- station_locs[which(station_locs$Project %in% c("SBL", "SBT", "KIRB")), ]
@@ -28,6 +23,11 @@ sl_red$Site <- substr(sl_red$Station, 1, nchar(sl_red$Station) - 3)
 sl_red_reordered <- sl_red[, c("Project", "Site", "Station", "Zone", "Easting", "Northing", "Elevation",
                                "Paired", "Type", "Channel")]
 
+####### Output ####################################
+
+# Create a connection to a database
+db <- DBI::dbConnect(RSQLite::SQLite(),
+                     "data/generated/localization.db")
 DBI::dbWriteTable(conn = db,
                   name = "stations",
                   value = sl_red_reordered,
