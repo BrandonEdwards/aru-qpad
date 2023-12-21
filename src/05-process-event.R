3f7ebb347bda4c20dc4780c7595df2550c25aab5
Brandon Edwards <brandon.edwards144@gmail.com>
2023-12-21 11:57:44 -0500

    can call a python script, but I'm going to have to think carefully about runnign multiple of these scripts at once as it seems to blow up the computer

--- src/05-process-event.R
+++ src/05-process-event.R
@@ -7,24 +7,58 @@
 
 ####### Import Libraries and External Files #######
 
+library(DBI)
+library(RSQLite)
+
 ####### Read Data #################################
 
 args <- commandArgs(trailingOnly=TRUE)
 
 ####### Main Code #################################
 
-message(paste0("Now processing event ", args))
+event <- gsub(pattern = "_", replacement = " ", x = args[1])
+message(paste0("Now processing event ", event))
+
+db <- DBI::dbConnect(RSQLite::SQLite(),
+                     "data/generated/recordings.db")
 
 
+isWAC <- dbGetQuery(conn = db,
+                    paste0("SELECT isWAC FROM events WHERE Event = \"", event, "\""))[,1]
+if (any(as.logical(isWAC)))
+{
+  # convert()
+}
 
-# Check for conversion
+isDetected <- dbGetQuery(db,
+                         paste0("SELECT isDetected FROM events WHERE Event = \"", 
+                                event,
+                                "\""))[,1]
+if (any(isDetected == 0))
+{
+  dir_out <- paste0("data/temp_files/", args[1], "/")
+  dir_string <- paste0(dir_out, "\\*.wav")
+  files <- dbGetQuery(db,
+                      paste0("SELECT Local_File FROM events WHERE Event = \"", 
+                             event,
+                             "\""))[,1]
+  for (f in files)
+  {
+    system(paste("python3 src/detect.py", dir_string, dir_out))
+  }
+}
 
-# Detect birds and output
-
-# Localize detected birds and output
+isLocalized <- dbGetQuery(db,
+                          paste0("SELECT isLocalized FROM events WHERE Event = \"", 
+                                 event,
+                                 "\""))[,1]
+if (any(isLocalized == 0))
+{
+  # Localize detected birds and output
+}
 
 # Change directory to DONE, or add a DONE file to it
 
-
+dbDisconnect(db)
 
 ####### Output ####################################
